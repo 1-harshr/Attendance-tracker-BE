44c08d478f4f6d667d88ce4dc447c0ad598de43d
harsh.ranjan <harsh.ranjan@travenues.com>
2025-07-10 19:59:13 +0530

    feat: configure PostgreSQL database connection
    - Add PostgreSQL driver dependency to build.gradle
    - Configure database connection properties for external PostgreSQL instance
    - Set JPA/Hibernate properties for PostgreSQL dialect

--- src/main/java/com/example/attendance_tracker/attendace_tracker/AttendanceTrackerApplication.java
+++ src/main/java/com/example/attendance_tracker/attendace_tracker/AttendanceTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AttendaceTrackerApplication {
+public class AttendanceTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AttendaceTrackerApplication.class, args);
+		SpringApplication.run(AttendanceTrackerApplication.class, args);
 	}
 
 }
