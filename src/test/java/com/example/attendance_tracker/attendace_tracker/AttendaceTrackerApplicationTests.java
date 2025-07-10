d2b910bbcc86f41488e5d6d23d3096154be13cf7
harsh.ranjan <harsh.ranjan@travenues.com>
2025-07-10 19:43:37 +0530

    initial commit

---
+++ src/test/java/com/example/attendance_tracker/attendace_tracker/AttendaceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendance_tracker.attendace_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendaceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
